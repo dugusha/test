49958faab7b05ffb0514fd9cf6fb40712bff0810
gaoruishen <gaoruishen@gaoruishen.local>
2019-04-30 13:45:40 +0800

    fix bug

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,9 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
+function p($x=["@222@"],$isNotExit = false){
+    header('Content-type: application/json');
+    echo json_encode($x);
+    if(!$isNotExit) exit;
+}
 return ComposerAutoloaderInit7892e30bae6c156073b334478bfbabcd::getLoader();
